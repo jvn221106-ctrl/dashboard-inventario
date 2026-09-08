9ef585cdc53d4f5427c5987a5161284a4d9e618f
jvn221106-ctrl <jvn221106@gmail.com>
2026-09-08 10:52:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -447,6 +447,7 @@ def renderizar_dashboard():
 
         # --- VISÃO COMPARATIVA REGIONAL (ADMIN E GERENTES REGIONAIS) ---
         if perfil_usuario == "Administrador":
+            
         st.subheader("🗺️ Comparativo por Divisão Regional (Regional 1 vs Regional 2)")
         
         df_reg_comp = (
