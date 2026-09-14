6b5526638fcf2710e9191bd8d8048c9b35418dc9
jvn221106-ctrl <jvn221106@gmail.com>
2026-09-14 11:22:36 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # Exemplo de link direto formatado corretamente:
-URL_EXCEL_NUVEM = "https://vonnycosmeticos.sharepoint.com/:x:/s/ControladoriaPrevenodePerdas-CONTROLADORIA-PREVENODEPERDAS/IQBInqh02m9tQ77Yk4SJ0QDVAaDaWAL5Sn-84UhgG8zBmlQ?download=1"
+URL_EXCEL_NUVEM = "https://vonnycosmeticos.sharepoint.com/:x:/s/ControladoriaPrevenodePerdas-CONTROLADORIA-PREVENODEPERDAS/IQBInqh02m9tQ77Yk4SJ0QDVAY8DSBYODhqr-xEh6V4Dqhc?download=1"
 
 DB_FILE = "usuarios_db.json"
 
